7b08273b353a342fd01f21285ab753139c1bb7cb
Alex Yusiuk <thepulticula@gmail.com>
2022-06-26 23:04:36 +0300

    move enviroment variabels from config.fish to enviroment.d

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,11 +5,9 @@ set PATH $HOME/.cargo/bin $PATH
 starship init fish | source
 alias ls="lsd"
 alias la="lsd -la"
+alias at="alacritty-themes"
 export NNN_PLUG='f:fd;o:fzopen;d:diffs;t:nmount;v:imgview;p:-plugin'
 alias nnn "nnn -e"
 set --export NNN_FIFO "/tmp/nnn.fifo"
 
-export SDL_VIDEODRIVER=wayland
-export _JAVA_AWT_WM_NONREPARENTING=1
-export QT_QPA_PLATFORM=wayland
-export XDG_SESSION_DESKTOP=sway
+fish_add_path /home/sasha/.spicetify
